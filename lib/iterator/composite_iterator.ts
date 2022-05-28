3d551b5a0ef25f9b8cb92fb86805e452313922de
ArtuKILL <39144443+ArtuKILL@users.noreply.github.com>
2022-05-28 01:37:32 -0400

    Console.log removido

--- lib/iterator/composite_iterator.ts
+++ lib/iterator/composite_iterator.ts
@@ -10,10 +10,11 @@ export class CompositeIterator<T> implements Iterator<BaseComponent<T>> {
 
   constructor(compositeCollection: CompositeCollection<T>) {
     this._stack.push(compositeCollection.root);
-    console.log(this.hasMore());
   }
 
   next(): BaseComponent<T> {
+    console.log("Iterator: next");
+
     if (this._stack.isEmpty()) {
       throw new Error("No more elements - stack is empty");
     }
